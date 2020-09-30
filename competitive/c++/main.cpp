8f7df2242b65ce4c5950a6f7647c051e3a23cea2
Syed Ahmad <muzammil.ahmad@gmail.com>
2020-09-30 19:36:58 +1000

    attempt 1

--- competitive/c++/main.cpp
+++ competitive/c++/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-int main() {
-  cout << "hello world?";
+int main()
+{
+
+  class Solution
+  {
+  public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+      for (int i = 0; i < nums.size(); ++i)
+      {
+        for (int z = 1; z < nums.size(); ++z)
+        {
+          if(nums[i] + nums[z] == target){
+            vector<int> s;
+            s.push_back(i);
+            s.push_back(z);
+            return s;
+          }
+        }
+      }
+    }
+  };
 }
