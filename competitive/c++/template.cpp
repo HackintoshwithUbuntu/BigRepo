59caee01ac2014e33605680bf02a49ffcbdfe5eb
Syed Ahmad <muzammil.ahmad@gmail.com>
2020-10-16 11:38:55 +1100

    added a template

---
+++ competitive/c++/template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int n, b, a[100000];
+
+void solve() {}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
