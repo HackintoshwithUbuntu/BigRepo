b2c17eac20279be1aebdc8c53605bde3291e7f6b
Syed Ahmad <muzammil.ahmad@gmail.com>
2020-10-18 11:41:59 +1100

    comp

--- competitive/c++/main.cpp
+++ competitive/c++/main.cpp
@@ -1,8 +1,37 @@
+/*
+// Sample code to perform I/O:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;										// Reading input from STDIN
+	cout << "Input number is " << num << endl;		// Writing output to STDOUT
+}
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+*/
+
+// Write your code here
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
+string s, t;
+
+void solve() {}
+
 int main()
-{}
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
