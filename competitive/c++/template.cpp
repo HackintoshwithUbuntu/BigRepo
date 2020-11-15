e9e4a2002c38da1ddfd364b65f8df4ab9940fecd
Syed Ahmad <muzammil.ahmad@gmail.com>
2020-11-15 12:45:45 +1100

    updated the template

--- competitive/c++/template.cpp
+++ competitive/c++/template.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int n, b, a[100000];
 
-void solve() {}
+void solve() {
+
+    int ans;
+    cout << ans << "\n";
+}
 
 int main()
 {
 
-    int t;
+    int t, i = 1;
     cin >> t;
-    while (t--)
-    {
+    while(t--)
+    {   
+        cout << "Case #" << i << ": ";
         solve();
+        ++i;
     }
 }
