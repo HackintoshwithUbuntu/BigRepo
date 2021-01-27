9412e1858465adca428edd0450c40b5cae0eac0e
HackintoshwithUbuntu <muzammil.ahmad@gmail.com>
2021-01-27 21:05:12 +1100

    updated helpercode

--- competitive/c++/helpercode.cpp
+++ competitive/c++/helpercode.cpp
@@ -1,74 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isEven(int n)
-{
-    // n&1 is 1, then odd, else even
-    // Change the 1 to any power of 2
-    return (n & 1);
+bool isEven(int n) {
+  // n&1 is 1, then odd, else even
+  // Change the 1 to any power of 2
+  return (n & 1);
 }
 
-int dividePowerOf2(int i)
-{
-    // Change the 1 to equal any power of 2
-    return i >> 1;
+int dividePowerOf2(int i) {
+  // Change the 1 to equal any power of 2
+  return i >> 1;
 }
 
-int mulitplyPowerOf2(int i)
-{
-    // Change the 1 to equal any power of 2
-    return i << 1;
+int mulitplyPowerOf2(int i) {
+  // Change the 1 to equal any power of 2
+  return i << 1;
 }
 
-int count_digit(int number)
-{
-    int count = 0;
-    while (number != 0)
-    {
-        number = number / 10;
-        count++;
+int count_digit(int number) {
+  int count = 0;
+  while (number != 0) {
+    number = number / 10;
+    count++;
+  }
+  return count;
+}
+// Alternate count_digit
+int countDigit(int i) {
+  string i_str;
+  i_str = to_string(i);
+  return (i_str.size());
+}
+
+vector<int> clean(vector<int> arrayc) {
+  for (int i = 0; i < arrayc.size() - 1; i++) {
+    if (arrayc[i] == arrayc[i + 1]) {
+      arrayc.erase(arrayc.begin() + i);
+      i--;
     }
-    return count;
-}
-//Alternate count_digit
-int countDigit(int i)
-{
-    string i_str;
-    i_str = to_string(i);
-    return (i_str.size());
+  }
+  return arrayc;
 }
 
-vector<int> clean(vector<int> arrayc)
-{
-    for (int i = 0; i < arrayc.size() - 1; i++)
-    {
-        if (arrayc[i] == arrayc[i + 1])
-        {
-            arrayc.erase(arrayc.begin() + i);
-            i--;
-        }
-    }
-    return arrayc;
-}
-
-//initalise full array
+// initalise full array
 bool trueorfalse[100] = {0};
 
-void returnalldigitsorcharacters(string str)
-{
-    for (auto c : str) 
-        //Checking if digit
-        if (isdigit(c)) break;
-        // vector.push_back(c);
+void returnalldigitsorcharacters(string str) {
+  for (auto c : str)
+    // Checking if digit
+    if (isdigit(c)) break;
+  // vector.push_back(c);
 }
 
 bool testIfCharacterInWord(char useIndexFromString) {
-  for(auto x: "aeiouAEIOU"){
-      // This will compare the string index (char) to each value of x, in this case all the vowels
-      // The string index can be iterated over in another loop
-      // if(x == useIndexFromString)
-      // RETURN TRUE;
-      break;
+  for (auto x : "aeiouAEIOU") {
+    // This will compare the string index (char) to each value of x, in this
+    // case all the vowels The string index can be iterated over in another loop
+    // if(x == useIndexFromString)
+    // RETURN TRUE;
+    break;
   }
   return false;
+}
+
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
 }
