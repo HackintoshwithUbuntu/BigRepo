a281d7d7b8d55952b7a0246c57b87bdbafac6020
HackintoshwithUbuntu <muzammil.ahmad@gmail.com>
2021-04-29 19:39:08 +1000

    stuff for cpp test

---
+++ competitive/c++/z.aio/test/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    freopen("testin.txt", "r", stdin);
+    freopen("testout.txt", "w", stdout);
+    
+    int a, b, ans;
+    cin >> a >> b;
+    ans = a * b;
+
+    cout << ans << "\n";
+}
