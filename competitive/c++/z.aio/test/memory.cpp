ddb7b302620b55ec96002b25c903b624aae35baf
muzammil.ahmad@gmail.com <muzammil.ahmad@gmail.com>
2021-07-03 22:37:29 +1000

    added test

---
+++ competitive/c++/z.aio/test/memory.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    freopen("ladyin.txt", "r", stdin);
+    freopen("ladyout.txt", "w", stdout);
+
+    while (true)
+      long double *a = new long double;  // allocating 
+}
